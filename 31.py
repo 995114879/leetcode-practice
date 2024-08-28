d5b5746f92e1b84d7954547d505c046574c246cd
gits <995114879@qq.com>
2024-08-28 10:02:42 +0800

    31

---
+++ 31.py
@@ -0,0 +1,34 @@
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        i = n-2
+        while i >= 0 and nums[i] >= nums[i + 1]:
+            i -= 1
+        if i >= 0:
+            j = n - 1
+            while j >= 0 and nums[i] >= nums[j]:
+                j -= 1
+            nums[i], nums[j] = nums[j], nums[i]
+        
+        left, right = i + 1, n - 1
+        while left < right:
+            nums[left], nums[right] = nums[right], nums[left]
+            left += 1
+            right -= 1
+
+            
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        for i in range(len(nums)-1,-1,-1):
+            for j in range(len(nums)-1,i,-1):
+                if nums[j]>nums[i]:
+                    nums[i],nums[j] = nums[j],nums[i]
+                    nums[i+1:] = nums[i+1:][::-1]
+                    return
+        nums.reverse()
